507c1247ec90f8db14d80f4f51a7fd7f3746c4e3
Tobias Schulz-Hess <github.com@schulz-hess.de>
2024-07-30 11:35:47 +0200

    Handling all scr relevant work in the thread. Make sure to run thread only once.

--- __init__.py
+++ __init__.py
@@ -12,11 +12,11 @@ ICON = "A:apps/webcam/resources/icon.png"
 
 # LVGL widgets
 scr = None
-label = None
 
 # App manager
 app_mgr = None
 task_running = False
+task_running_lock = _thread.allocate_lock()
 
 # Constants
 DEFAULT_BG_COLOR = lv.color_hex3(0x000)
@@ -25,63 +25,85 @@ DEFAULT_BG_COLOR = lv.color_hex3(0x000)
 webcam_index = 0
 
 def load_webcam():
-    global scr, label, webcam_index, task_running
+    global scr, webcam_index, task_running, task_running_lock
     
     error = False
     
-    try:
-        while (task_running and not error):
-            if scr:
-                s = app_mgr.config()
-                url = s.get(f"url{webcam_index + 1}", "Unknown")
-                if url.startswith("http"):
-                    if net.connected():
-                        response = None
-                        if '@' in url and ':' in url:
-                            # we need to do basic auth
-                            start = url.index(':') + 3
-                            end = url.index('@')
-                            usernamepassword = url[start:end]
-                            url = url[:start] + url[(end + 1):]
-                            if ':' in usernamepassword:
-                                sep_index = usernamepassword.index(':')
-                                username = usernamepassword[:sep_index]
-                                password = usernamepassword[(sep_index + 1):]
-                                print(f"Calling {url} with Username {username} and given password") 
-                                response = urequests.get(url, auth=(username,password))
-                        else:
-                            print(f"Calling {url} without basic auth")
-                            response = urequests.get(url)
-                        
-                        print(f"Got image from {url} with response {response.status_code}")
+    scr = lv.obj()
+    lv.scr_load(scr)
 
-                        if response is not None and response.status_code == 200:                
-                            image_description = lv.img_dsc_t()
-                            image_description.data = response.content
-                            image_description.data_size = len(response.content)
-                            
-                            image = lv.img(scr)
-                            image.set_src(image_description)
-                            image.center()
-                            
-                            scr.set_style_bg_color(DEFAULT_BG_COLOR,0)
-                        else:
-                            if response is None:
-                                label.set_text(f"Error URL wrongly formatted")
+    scr.set_style_bg_color(DEFAULT_BG_COLOR, lv.PART.MAIN)    
+    label = lv.label(scr)
+    label.center()
+    label.set_text(f"Loading webcam {webcam_index + 1}...")
+    lv.scr_load(scr)
+
+    # Focus the key operation on the current screen and enable editing mode.
+    lv.group_get_default().add_obj(scr)
+    lv.group_focus_obj(scr)
+    lv.group_get_default().set_editing(True)
+    scr.add_event(event_handler, lv.EVENT.ALL, None)
+    
+    with task_running_lock:
+        time.sleep_ms(800)  # Allow other tasks to run
+        try:
+            while (task_running and not error):
+                if scr:
+                    s = app_mgr.config()
+                    url = s.get(f"url{webcam_index + 1}", "Unknown")
+                    if url.startswith("http"):
+                        if net.connected():
+                            response = None
+                            if '@' in url and ':' in url:
+                                # we need to do basic auth
+                                start = url.index(':') + 3
+                                end = url.index('@')
+                                usernamepassword = url[start:end]
+                                url = url[:start] + url[(end + 1):]
+                                if ':' in usernamepassword:
+                                    sep_index = usernamepassword.index(':')
+                                    username = usernamepassword[:sep_index]
+                                    password = usernamepassword[(sep_index + 1):]
+                                    print(f"Calling {url} with Username {username} and given password") 
+                                    response = urequests.get(url, auth=(username,password))
                             else:
-                                label.set_text(f"Error {response.status_code} while loading {url}")
+                                print(f"Calling {url} without basic auth")
+                                response = urequests.get(url)
+                                
+                            if response is not None:
+                                print(f"Got image with response {response.status_code}")
+                            
+                            if task_running:
+                                if response is not None and response.status_code == 200:                
+                                    image_description = lv.img_dsc_t()
+                                    image_description.data = response.content
+                                    image_description.data_size = len(response.content)
+                                    
+                                    image = lv.img(scr)
+                                    image.set_src(image_description)
+                                    image.center()
+                                    
+                                    scr.set_style_bg_color(DEFAULT_BG_COLOR,0)
+                                    
+                                    response.close()
+                                else:
+                                    if response is None:
+                                        label.set_text(f"Error URL wrongly formatted")
+                                    else:
+                                        label.set_text(f"Error {response.status_code} while loading {url}")
+                                    lv.scr_load(scr)
+                        else:
+                            label.set_text(f"Wifi not connected")
                             lv.scr_load(scr)
                     else:
-                        label.set_text(f"Wifi not connected")
+                        label.set_text(f"Please configure webcams in application settings")
                         lv.scr_load(scr)
-                else:
-                    label.set_text(f"Please configure webcams in application settings")
-                    lv.scr_load(scr)
-                    error = True
-            time.sleep_ms(100)  # Allow other tasks to run
-    except:
-        print('Webcam thread had an exception')
-        raise
+                        error = True
+                if task_running:
+                    time.sleep_ms(100)  # Allow other tasks to run
+        except:
+            print('Webcam thread had an exception')
+            raise
     print("Webcam thread ended")
 
 def change_webcam(delta):
@@ -122,20 +144,25 @@ def event_handler(event):
         if not lv.group_get_default().get_editing():
             lv.group_get_default().set_editing(True)
 
+
 async def on_boot(apm):
     global app_mgr
     app_mgr = apm
     
+    
 async def on_resume():
     print('on resume')
-    global task_running
-    task_running = True
-    _thread.start_new_thread(load_webcam, ())
+    global task_running, task_running_lock
+    if not task_running_lock.locked():
+        task_running = True
+        _thread.start_new_thread(load_webcam, ())
+    
     
 async def on_pause():
     print('on pause')
     global task_running
     task_running = False    
+    
     
 async def on_stop():
     print('on stop')
@@ -145,27 +172,12 @@ async def on_stop():
         scr.clean()
         scr.del_async()
         scr = None
+        
 
 async def on_start():
     print('on start')
-    global scr, label
-    scr = lv.obj()
-    lv.scr_load(scr)
-
-    scr.set_style_bg_color(DEFAULT_BG_COLOR, lv.PART.MAIN)    
-    label = lv.label(scr)
-    label.center()
-    label.set_text("Loading webcam 1...")
-    lv.scr_load(scr)
-
-    scr.add_event(event_handler, lv.EVENT.ALL, None)
-
-    # Focus the key operation on the current screen and enable editing mode.
-    lv.group_get_default().add_obj(scr)
-    lv.group_focus_obj(scr)
-    lv.group_get_default().set_editing(True)
-
-        
+    
+    
 def get_settings_json():
     return {
         "title":"Settings for Webcam app",
