ddc60ff9883fb699fd24a1ebeba92110aea18bea
Tobias Schulz-Hess <github.com@schulz-hess.de>
2024-08-05 14:41:02 +0200

    Showing name for Webcams and more typing

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+from typing import Any
 import lvgl as lv
 import urequests
 import net
@@ -5,17 +6,17 @@ import _thread
 import time
 
 # App Name
-NAME = "Webcam"
+NAME: str = "Webcam"
 
 # App Icon
-ICON = "A:apps/webcam/resources/icon.png"
+ICON: str = "A:apps/webcam/resources/icon.png"
 
 # LVGL widgets
-scr = None
-label = None
+scr: lv.obj = None
+label: lv.obj = None
 
 # App manager
-app_mgr = None
+app_mgr: Any = None
 task_running: bool = False
 task_running_lock = _thread.allocate_lock()
 
@@ -24,12 +25,13 @@ DEFAULT_BG_COLOR = lv.color_hex3(0x000)
 
 # Current image index
 webcam_index: int = 0
+webcam_name: str = ""
 webcam_changed: bool = False
 
 DEBUG: bool = False
 
 
-def dprint(msg: str):
+def dprint(msg: str) -> None:
     """
     Print a debug message to console, if in debug mode.
 
@@ -40,7 +42,7 @@ def dprint(msg: str):
         print(msg)
 
 
-def load_image_from_url(url: str):
+def load_image_from_url(url: str) -> None:
     """
     Actually load an image from a given URL.
 
@@ -105,7 +107,7 @@ def load_image_from_url(url: str):
         raise Exception(f"Please configure webcams in application settings")
 
 
-def load_webcam():
+def load_webcam() -> None:
     """
     Task to manage loading the webcam images and displaying them.
 
@@ -120,10 +122,13 @@ def load_webcam():
     scr.set_style_bg_color(DEFAULT_BG_COLOR, lv.PART.MAIN)
     scr.set_style_bg_img_src("A:apps/webcam/resources/bg.png", lv.PART.MAIN)
 
+    app_mgr_config = app_mgr.config()
+    webcam_name = app_mgr_config.get(f"name{webcam_index + 1}", "")
+
     if label is None:
         label = lv.label(scr)
         label.center()
-    label.set_text(f"Loading webcam {webcam_index + 1}...")
+    label.set_text(f"Loading webcam {webcam_index + 1}...\n{webcam_name}")
 
     # Focus the key operation on the current screen and enable editing mode.
     lv.group_get_default().add_obj(scr)
@@ -137,8 +142,7 @@ def load_webcam():
         time.sleep_ms(800)  # Allow other tasks to run
         try:
             while task_running:
-                s = app_mgr.config()
-                url = s.get(f"url{webcam_index + 1}", "Unknown")
+                url = app_mgr_config.get(f"url{webcam_index + 1}", "Unknown")
 
                 try:
                     image_description = load_image_from_url(url)
@@ -162,7 +166,7 @@ def load_webcam():
     dprint("Webcam thread ended")
 
 
-def change_webcam(delta: int):
+def change_webcam(delta: int) -> None:
     """
     Change the webcam.
 
@@ -174,21 +178,22 @@ def change_webcam(delta: int):
 
     global webcam_index, app_mgr, scr, label, webcam_changed
 
-    s = app_mgr.config()
+    app_mgr_config = app_mgr.config()
     webcam_changed = True
 
     while True:
         webcam_index = (webcam_index + delta) % 5
 
         # Check if URL is valid:
-        url = s.get(f"url{webcam_index + 1}", "Unknown")
+        url = app_mgr_config.get(f"url{webcam_index + 1}", "Unknown")
+        webcam_name = app_mgr_config.get(f"name{webcam_index + 1}", "")
         if url.startswith("http") or webcam_index == 0:
             scr.set_style_bg_img_src("A:apps/webcam/resources/bg.png", lv.PART.MAIN)
-            label.set_text(f"Loading webcam {webcam_index + 1}...")
+            label.set_text(f"Loading webcam {webcam_index + 1}...\n{webcam_name}")
             break
 
 
-def event_handler(event):
+def event_handler(event) -> None:
     """
     Code executed when an event is called.
 
@@ -215,7 +220,7 @@ def event_handler(event):
             lv.group_get_default().set_editing(True)
 
 
-async def on_boot(apm):
+async def on_boot(apm: Any) -> None:
     """
     Code executed on boot.
     See https://dock.myvobot.com/developer/guides/app-design/ for clife cycle diagram
@@ -224,7 +229,7 @@ async def on_boot(apm):
     app_mgr = apm
 
 
-async def on_resume():
+async def on_resume() -> None:
     """
     Code executed on resume. Essentially this starts the webcam thread.
 
@@ -243,7 +248,7 @@ async def on_resume():
     _thread.start_new_thread(load_webcam, ())
 
 
-async def on_pause():
+async def on_pause() -> None:
     """
     Code executed on pause. This stops the webcam thread.
 
@@ -254,7 +259,7 @@ async def on_pause():
     task_running = False
 
 
-async def on_stop():
+async def on_stop() -> None:
     """
     Code executed on stop. Make sure, everything is cleaned up nicely.
 
@@ -278,18 +283,16 @@ async def on_stop():
         label = None
 
 
-async def on_start():
+async def on_start() -> None:
     """
     Code executed on start.
 
     See https://dock.myvobot.com/developer/guides/app-design/ for clife cycle diagram
     """
     dprint("on start")
-    scr = None
-    label = None
 
 
-def get_settings_json():
+def get_settings_json() -> dict:
     """
     App settings.
 
@@ -310,10 +313,24 @@ def get_settings_json():
             {
                 "type": "input",
                 "default": "",
+                "caption": "Name for webcam 1",
+                "name": "name1",
+                "attributes": {"placeholder": "Frontdoor"},
+            },
+            {
+                "type": "input",
+                "default": "",
                 "caption": "URL for webcam 2:",
                 "name": "url2",
                 "tip": "Use http://{USERNAME}:{PASSWORD}@my.domain/webcam.jpg for Basic Auth.",
                 "attributes": {"placeholder": "http://my.domain/webcam.jpg"},
+            },
+            {
+                "type": "input",
+                "default": "",
+                "caption": "Name for webcam 2",
+                "name": "name2",
+                "attributes": {"placeholder": "Frontdoor"},
             },
             {
                 "type": "input",
@@ -326,6 +343,13 @@ def get_settings_json():
             {
                 "type": "input",
                 "default": "",
+                "caption": "Name for webcam 3",
+                "name": "name3",
+                "attributes": {"placeholder": "Frontdoor"},
+            },
+            {
+                "type": "input",
+                "default": "",
                 "caption": "URL for webcam 4:",
                 "name": "url4",
                 "tip": "Leave empty, if not used.",
@@ -334,10 +358,24 @@ def get_settings_json():
             {
                 "type": "input",
                 "default": "",
+                "caption": "Name for webcam 4",
+                "name": "name4",
+                "attributes": {"placeholder": "Frontdoor"},
+            },
+            {
+                "type": "input",
+                "default": "",
                 "caption": "URL for webcam 5:",
                 "name": "url5",
                 "tip": "Leave empty, if not used.",
                 "attributes": {"placeholder": "http://my.domain/webcam.jpg"},
+            },
+            {
+                "type": "input",
+                "default": "",
+                "caption": "Name for webcam 5",
+                "name": "name5",
+                "attributes": {"placeholder": "Frontdoor"},
             },
         ],
     }
